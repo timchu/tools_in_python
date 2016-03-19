20b2a6b2d63fe62837d8c68bcc46d1a35adefad7
timchu <timchu>
2016-03-19 08:22:56 -0400

    Added a timer

---
+++ timer.py
@@ -0,0 +1,22 @@
+import time
+import os
+
+def print_command(thing_to_print):
+    return "printf '" + str(thing_to_print) + "'"
+
+def spawn_terminal(command):
+    os.system("gnome-terminal -e 'bash -c \"" + command + "; exec bash \"'")
+
+t = raw_input("Enter time in minutes: ")
+time.sleep(60 * float(t))
+spawn_terminal(print_command("TIME!"))
+
+
+#Known error: There's some stupid string escaping business that prevents me from
+# printing "TIME IS UP".  But I don't have the patience to find it.
+
+# More functions with string escaping errors.
+# def multi_print_command(thing_to_print, k):
+#     return "printf '" + thing_to_print + "\\n%.0s' {1.." + str(k) + "}"
+# Example command:
+# command = printf 'HelloWorld\n%.0s' {1..5}
